5628437a6b5a14c7afc4402d2694ba7bb49a488d
Tudor Maxim <maximtudor98@gmail.com>
2021-03-01 20:13:52 +0200

    Initialized repository

---
+++ android/app/src/main/kotlin/financialsystemscoursework/financial_systems_coursework/MainActivity.kt
@@ -0,0 +1,12 @@
+package financialsystemscoursework.financial_systems_coursework
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
