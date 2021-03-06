3941609b9729451cf7d25f2d4aafa53d1fc63322
Teofana-Ionela Moisi <teofana.moisi@gmail.com>
2021-03-06 12:22:34 +0200

    Stock and Formulae model classes implemented

--- lib/model/Stock.dart
+++ lib/model/Stock.dart
@@ -1,3 +1,31 @@
 class Stock {
-  // TODO: Add stock properties and methods.
+  String symbol;
+  int timestamp;
+  double currentMarketPrice;
+
+  Stock({this.symbol, this.timestamp, this.currentMarketPrice});
+
+  String getSymbol() {
+    return this.symbol;
+  }
+
+  int getTimeStamp() {
+    return this.timestamp;
+  }
+
+  double getCurrentMarketPrice() {
+    return this.currentMarketPrice;
+  }
+
+  void setSymbol(String symbol) {
+    this.symbol = symbol;
+  }
+
+  void setTimestamp(int timestamp) {
+    this.timestamp = timestamp;
+  }
+
+  void setCurrentMarketPrice(double currentMarketPrice) {
+    this.currentMarketPrice = currentMarketPrice;
+  }
 }
