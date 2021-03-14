60adfa6a7419964a48666fa2295738bcbde47c39
Horia <horiapavel@yahoo.com>
2021-03-14 16:54:31 +0000

    Make some functionality private

--- lib/repository/StockDataCollector.dart
+++ lib/repository/StockDataCollector.dart
@@ -18,7 +18,7 @@ class StockDataCollector {
   }
 
   /// Craft URL
-  static String createURL(String symbol, String startDate, String endDate) {
+  String _createURL(String symbol, String startDate, String endDate) {
     return "https://query1.finance.yahoo.com/v8/finance/chart/" + symbol +
         "?symbol=" + symbol +
         "&period1=" + startDate +
@@ -41,14 +41,14 @@ class StockDataCollector {
    */
   Future<List<Stock>> getPrices(String symbol, String startDate, String endDate) async {
     final response = await http.get(
-      createURL(symbol, startDate, endDate),
+      _createURL(symbol, startDate, endDate),
       headers: <String, String> {
         'Content-Type': 'application/json',
       },
     );
     if (response.statusCode == 200) {
       var jsonResponse = jsonDecode(response.body);
-      return jsonToStocks(symbol, jsonResponse);
+      return _jsonToStocks(symbol, jsonResponse);
 
     } else {
       return null;
@@ -61,7 +61,7 @@ class StockDataCollector {
    *
    * Take closing price of each day.
    */
-  static List<Stock> jsonToStocks(String symbol, dynamic jsonObject) {
+  List<Stock> _jsonToStocks(String symbol, dynamic jsonObject) {
 
     List<int> timestamps = (jsonObject['chart']['result'][0]['timestamp'] as List).cast<int>().toList();
     List<double> prices = (jsonObject['chart']['result'][0]['indicators']['quote'][0]['close'] as List).cast<double>().toList();
