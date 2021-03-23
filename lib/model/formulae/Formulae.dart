1b2e531a566e536435e76ac00a699baf22ee15b4
Horia <32480920+Horiapavel98@users.noreply.github.com>
2021-03-23 18:19:28 +0200

    Correct typo

--- lib/model/formulae/Formulae.dart
+++ lib/model/formulae/Formulae.dart
@@ -3,7 +3,7 @@ import '../Point.dart';
 
 abstract class Formulae {
   static final String periodError =
-      'Period must be smalled than the number of stocks';
+      'Period must be smaller than the number of stocks';
 
   List<Point> compute(List<Stock> stocks, int period, int startDate);
 }
